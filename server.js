6ab20e41403ea84cf4c01ae0c98943f9ed3f711c
sococa <pierrocarlier@gmail.com>
2023-04-14 10:34:52 +0200

    suppression corsOption

--- server.js
+++ server.js
@@ -1,40 +1,36 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const expressFormidable = require('express-formidable')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const app = express()
+const express = require("express");
+const expressFormidable = require("express-formidable");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
 
-app.use(expressFormidable())
-app.use(cors(corsOptions))
-
-const corsOptions = {
-    origin: 'http://localhost:3001'
-}
+app.use(expressFormidable());
+app.use(cors(corsOptions));
 
 try {
-    mongoose.connect(process.env.MONGO_URI)
-    console.log("connexion à la bdd réussie")
-} catch(error) {
-    console.log("ERREUR : ", error.message)
+  mongoose.connect(process.env.MONGO_URI);
+  console.log("connexion à la bdd réussie");
+} catch (error) {
+  console.log("ERREUR : ", error.message);
 }
 
 // routes
-const register = require("./routes/user")
-const customerFeedback = require("./routes/customerFeedback")
-const education = require("./routes/education")
-const dogSitting = require("./routes/dogSitting")
-const tip = require("./routes/tip")
-const admin = require("./routes/admin")
+const register = require("./routes/user");
+const customerFeedback = require("./routes/customerFeedback");
+const education = require("./routes/education");
+const dogSitting = require("./routes/dogSitting");
+const tip = require("./routes/tip");
+const admin = require("./routes/admin");
 
-app.use(register)
-app.use(customerFeedback)
-app.use(education)
-app.use(dogSitting)
-app.use(tip)
-app.use(admin)
+app.use(register);
+app.use(customerFeedback);
+app.use(education);
+app.use(dogSitting);
+app.use(tip);
+app.use(admin);
 
-app.listen(3100, ()=> {
-    console.log('serveur en marche')
-})
+app.listen(3100, () => {
+  console.log("serveur en marche");
+});
